0b52c05f6be86e2f31a976ed194f11650dd2e39c
Sajan Mahla <sajanmahla965@gmail.com>
2025-08-04 10:52:24 +0530

    Update Constants.swift

--- WeatherAppWithAPI/Helpers/Constants.swift
+++ WeatherAppWithAPI/Helpers/Constants.swift
@@ -5,6 +5,6 @@
 //  Created by SAJAN  on 03/08/25.
 //
 struct Constants {
-    static let apiKey = "eca3d418b6d3c6168d995f37c1d25cad"
+    static let apiKey = "CANT SHOW THIS USE YOUR OWN "
     static let baseURL = "https://api.openweathermap.org/data/2.5/weather"
 }
